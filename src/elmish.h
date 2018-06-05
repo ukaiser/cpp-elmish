4ce396d967498d3f6957d8adcde9573bfa8b1389
ks <uwe.kaiser@brel.ch>
2018-06-05 16:33:09 +0200

    Add subsciptions to the store class

--- src/elmish.h
+++ src/elmish.h
@@ -1,6 +1,82 @@
 #ifndef SRC_ELMISCH_H
 #define SRC_ELMISCH_H
 
+#include <tuple>
+#include <variant>
+#include <vector>
+#include <functional>
+
+
+namespace impl
+{
+    template <typename... Ts>
+    using cat = decltype(std::tuple_cat(std::declval<Ts>()...));
+
+    template <typename TResult, typename... Ts>
+    struct flatten_variant;
+}
+
+namespace impl
+{
+    template <typename TResult>
+    struct flatten_variant<TResult>
+    {
+        using type = TResult;
+    };
+
+    template <typename TResult, typename T, typename... TOther>
+    struct flatten_variant<TResult, T, TOther...>
+    {
+        using type = cat<TResult, std::tuple<T>,
+                         typename flatten_variant<TResult, TOther...>::type>;
+    };
+
+    template <typename TResult, typename... Ts, typename... TOther>
+    struct flatten_variant<TResult, std::variant<Ts...>, TOther...>
+    {
+        using type =
+            cat<TResult, typename flatten_variant<std::tuple<>, Ts...>::type,
+                typename flatten_variant<TResult, TOther...>::type>;
+    };
+    
+    template<typename T>
+    struct to_variant;
+    
+    template<typename... Ts>
+    struct to_variant<std::tuple<Ts...>>
+    {
+        using type = std::variant<Ts...>;
+    };
+
+    template<typename T>
+    using flatten_variant_t = typename to_variant<typename flatten_variant<std::tuple<>, T>::type>::type;
+
+    template<typename T>
+    using flatten_variant_to_tuple_t = typename flatten_variant<std::tuple<>, T>::type;
+
+
+    template<typename T, typename Tuple>
+    struct TupleIndex;
+
+    template<typename T, typename... Ts>
+    struct TupleIndex<T, std::tuple<T, Ts...>>
+    {
+        static constexpr std::size_t value = 0;
+    };
+
+    template<typename T, typename U, typename... Ts>
+    struct TupleIndex<T, std::tuple<U, Ts...>>
+    {
+        static constexpr std::size_t value = 1 + TupleIndex<T, std::tuple<Ts...>>::value;
+    };
+
+    //template<typename T>
+
+}
+
+
+
+
 
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
@@ -57,8 +133,54 @@ std::string actionToString(const T& arg)
 template<typename Model, typename Action>
 class Store
 {
+    using FlattenedActions = impl::flatten_variant_to_tuple_t<Action>;
+
+    template<typename A>
+    using ActionCallback = std::function<void(const A& action, const Model& model)>;
+/*
+    template<typename A>
+    struct AugmentActionWithVectorOfCallbacks 
+    { 
+        using type = std::vector<ActionCallback<A>>;
+    };
+*/
+    template<typename A>
+    using AugmentActionWithVectorOfCallbacks = std::vector<ActionCallback<A>>;
+
+/*
+    template<typename T>
+    struct AugmentTuple;
+
+    template<typename T>
+    struct AugmentTuple<std::tuple<T>>
+    {
+        //using type = std::tuple<AugmentActionWithVectorOfCallbacks<T>>;
+        using type = std::tuple<T>;
+    };
+
+    template<typename T, typename... Ts>
+    struct AugmentTuple<std::tuple<T, Ts...>>
+    {
+        using type = impl::cat<std::tuple<T>, AugmentTuple<std::tuple<Ts...>>::type>;
+    };
+*/
+
+
+    template<typename T>
+    struct AugmentTuple;
+
+    template<typename... Ts>
+    struct AugmentTuple<std::tuple<Ts...>>
+    {
+        using type = std::tuple<AugmentActionWithVectorOfCallbacks<Ts>...>;
+    };
+
+    using TupleOfVectorsWithCallbacks = typename AugmentTuple<FlattenedActions>::type;
+
 public:
 
+
+    // return by value? 
 	const Model& model() const noexcept
 	{
 		return _model;
@@ -68,10 +190,41 @@ public:
 	{
         std::cout << actionToString(action) << "\n";
         updateModel(_model, action);
+        notify(action);
 	}
 
+
+    template<typename A>
+    void subscribe(ActionCallback<A> f)
+    {
+        std::get<impl::TupleIndex<A, FlattenedActions>::value>(_listener).push_back(f);
+    }
+
+
 private:
+
+    template<typename ...As>
+    void notify(const std::variant<As...>&  action)
+    {
+        return std::visit([this](auto&& arg){
+            return notify(arg);
+        }, action);
+    }
+
+
+    template<typename A>
+    void notify(const A&  action)
+    {
+        auto&& actionListeners = std::get<impl::TupleIndex<A, FlattenedActions>::value>(_listener);
+        for(auto&& listener : actionListeners)
+        {
+            listener(action, _model);
+        } 
+    }
+
+
 	Model _model;
+    TupleOfVectorsWithCallbacks _listener;
 };
 
 #endif  //SRC_ELMISCH_H
--- src/main.cpp
+++ src/main.cpp
@@ -23,11 +23,20 @@ using AppStore = Store<AppModel, AppAction>;
 int main()
 {
   AppStore appStore;
-	
+	auto test = [](const IncrementBy& incrementBy, const AppModel& model){
+    std::cout << "IncremtnBy Subscription called: " << incrementBy.value << " to: " << model.countModel.counter << "\n";
+  };
+  appStore.subscribe<IncrementBy>(test);
+
+  appStore.subscribe<CameraConnected>([](auto&&, auto&& ){std::cout << "CameraConnected! \n";});
+  appStore.subscribe<CameraDisconnected>([](auto&&, auto&&){std::cout << "CameraDisconnected! \n";});
+
+
   appStore.update(IncrementBy{2});
   std::cout << "counter: " << appStore.model().countModel.counter << '\n';
 
   appStore.update(IncrementBy{3});
+
   std::cout << "counter: " << appStore.model().countModel.counter << '\n';
 
   std::cout << "isConnected: "
@@ -40,6 +49,8 @@ int main()
 
   appStore.update(DecrementBy{4});
   std::cout << "counter: " << appStore.model().countModel.counter << '\n';
+
+  appStore.update(CameraDisconnected{});
 }
 
 
